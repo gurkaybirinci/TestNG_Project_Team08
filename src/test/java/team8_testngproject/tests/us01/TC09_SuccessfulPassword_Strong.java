4798ceb51054ee00e04b0126137916cc52a54901
MeryemBor <munirebor@gmail.com>
2023-02-21 14:20:45 +0000

    Meryem 2023.02.21

--- src/test/java/team8_testngproject/tests/us01/TC09_SuccessfulPassword_Strong.java
+++ src/test/java/team8_testngproject/tests/us01/TC09_SuccessfulPassword_Strong.java
@@ -1,30 +1,37 @@
 package team8_testngproject.tests.us01;
 
 import com.github.javafaker.Faker;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import team8_testngproject.pages.P01_HomePage;
-import team8_testngproject.pages.P02_RegisterPage;
+import team8_testngproject.pages.P04_MyAccountPage;
 import team8_testngproject.utilities.ConfigReader;
 import team8_testngproject.utilities.Driver;
+import team8_testngproject.utilities.ReusableMethods;
+
+import static team8_testngproject.utilities.Driver.driver;
 
 public class TC09_SuccessfulPassword_Strong {
 @Test
     public void successfulRegistration_StrongPassward() {
-        Driver.getDriver().get(ConfigReader.getProperty("URL"));
+    Driver.getDriver().get("https://hubcomfy.com/my-account-2/");
         Faker faker = new Faker();
 
-        P01_HomePage homePage = new P01_HomePage();
-        homePage.userRegisterButton.click();
+     P01_HomePage homePage = new P01_HomePage();
+    ReusableMethods.jsClick(homePage.myAccountButton);
 
-        P02_RegisterPage registerPage = new P02_RegisterPage();
-        registerPage.userNameBox.sendKeys(faker.name().username());
-        registerPage.e_mailBox.sendKeys(faker.internet().emailAddress());
-        registerPage.userPasswordBox.sendKeys(ConfigReader.getProperty("generatedPassword5"));
-        registerPage.policyAgreementBox.click();
-        registerPage.userSignUpButton.click();
+    P04_MyAccountPage myAccountPage = new P04_MyAccountPage();
+    myAccountPage.myAccountSiginUpOnPopup.click();
+    myAccountPage.myAccountUserNameReg.sendKeys(faker.name().username());
+    myAccountPage.myAccountUserEmailReg.sendKeys(faker.internet().emailAddress());
+    myAccountPage.myAccountPasswordReg.sendKeys(ConfigReader.getProperty("generatedPassword5"));
+    myAccountPage.myAccountPolicyCheckReg.click();
 
-        Assert.assertTrue(homePage.homePageLogo.isDisplayed());
+    Assert.assertTrue(myAccountPage.notification_Strong.isDisplayed());
+
+   Actions actions = new Actions(driver);
+    actions.doubleClick(myAccountPage.myAccountSiginUpOnPopup);
 
     }
 }
