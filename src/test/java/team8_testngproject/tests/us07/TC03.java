cc93cd0610fae5e88c4358aa3f055a631460c6c3
Gonca Saygili <goncasaygili@gmail.com>
2023-02-23 18:16:46 +0300

    Gonca-2023.02.23

--- src/test/java/team8_testngproject/tests/us07/TC03.java
+++ src/test/java/team8_testngproject/tests/us07/TC03.java
@@ -34,7 +34,7 @@ public class TC03 {
         loginPage.search.sendKeys("bag");
         loginPage.aramaTusu.click();
         ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].click();",
-                loginPage.cantaTheMono);
+                loginPage.sunnyBaby);
         ReusableMethods.waitFor(2);
         loginPage.bosSayfa.click();
         ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].click();",
