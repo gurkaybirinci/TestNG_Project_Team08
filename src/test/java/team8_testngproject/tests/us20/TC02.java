ff28b40cc23e11e12b1a5077f80aba11f2ab7e6c
Gürkay Birinci <gurkaybirinci@gmail.com>
2023-02-24 23:07:15 +0300

    Gurkay-2023.02.24

--- src/test/java/team8_testngproject/tests/us20/TC02.java
+++ src/test/java/team8_testngproject/tests/us20/TC02.java
@@ -1,15 +1,21 @@
 package team8_testngproject.tests.us20;
 
+import com.aventstack.extentreports.ExtentTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import team8_testngproject.pages.*;
 import team8_testngproject.utilities.ConfigReader;
 import team8_testngproject.utilities.Driver;
+import team8_testngproject.utilities.RaporlamaUtil;
 import team8_testngproject.utilities.ReusableMethods;
 
-public class TC02 { // Description alanına metin girilebilmeli (PASS)
-    @Test
+public class TC02 { // Description metin kutusuna metin girilebilmeli (PASS)
+    private final String testName = "US20 || TC02-Description Metin Kutusu";
+    private final String description = "Description metin kutusuna veri girilebilmeli";
+    private final String raporMesaji = "Description metin kutusuna veri girilebildiği doğrulanmıştır.";
+    @Test(testName = testName, description = "<span style='font-weight:bold'>Amaç:</span> " + description)
     public void tc01(){
+        ExtentTest extentTest = RaporlamaUtil.extentTest;
         P01_HomePage homePage = new P01_HomePage();
         P03_LoginPage loginPage = new P03_LoginPage();
         P04_MyAccountPage myAccountPage = new P04_MyAccountPage();
@@ -21,16 +27,21 @@ public class TC02 { // Description alanına metin girilebilmeli (PASS)
         loginPage.userNameGur.sendKeys(ConfigReader.getProperty("usernameGur"));
         loginPage.passwordGur.sendKeys(ConfigReader.getProperty("passwordGur"));
         loginPage.signInButtonGur.click();
+        extentTest.info("Login işlemi yapıldı.");
 
         homePage.signOutGur.click();
         myAccountPage.storeManagerGur.click();
         ReusableMethods.hover(vendorStoreManagerPage.couponsButtonGur);
         vendorStoreManagerPage.couponsAddNewButtonGur.click();
+        extentTest.info("Coupon ekleme sayfasına girildi.");
 
         String descriptionData = "Bu kupon size özeldir.";
         vendorCouponsPage.descriptionBoxGur.sendKeys(descriptionData);
+        extentTest.info("Description metin kutusuna veri girilebilirliği kontrol edildi.");
 
         Assert.assertEquals(vendorCouponsPage.descriptionBoxGur.getAttribute("value"), descriptionData);
         Driver.closeDriver();
+        RaporlamaUtil.message = "<span style='color:green; font-weight:bold; font-size: 14px'>TEST SONUCU: </span><br><span style='color:purple; font-size: 16px'>" + raporMesaji + "</span>";
+
     }
 }
