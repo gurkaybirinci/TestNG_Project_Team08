dfd417e5ee786994cb51124ff2248e189c8179a4
Mustafa Bacanak <bacanak117@gmail.com>
2023-02-27 13:01:14 +0300

    Mustafa son

--- src/test/java/team8_testngproject/tests/us06/UserStory_06.java
+++ src/test/java/team8_testngproject/tests/us06/UserStory_06.java
@@ -1,6 +1,5 @@
 package team8_testngproject.tests.us06;
 
-import com.aventstack.extentreports.ExtentTest;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,10 +9,7 @@ import team8_testngproject.utilities.Driver;
 import team8_testngproject.utilities.RaporlamaUtil;
 
 public class UserStory_06 {
-
-    private final String testName = "US-06 End to End Test";
-//    private final String description = "Kullanıcı siteden alışveriş yapabilmeli";
-    private final String raporMesaji = "Gereksinimler sağlandığı görüntülendi";
+    private final String raporMesaji = "Gereksinimlerin sağlandığı görüntülendi";
 
     P01_HomePage homePage;
     P07_ShoppingPage shoppingPage;
@@ -34,10 +30,8 @@ public class UserStory_06 {
          orderCompletePage=new P11_OrderCompletePage();
     }
 
-
-    @Test(testName = testName, description = "<span style='font-weight:bold'>Amaç:</span> " + "Search Box inputunun kullanılabilmesi")
+    @Test (testName = "Search Box Test", description = "<span style='font-weight:bold'>Amaç:</span> " + "Search Box inputunun kullanılabilmesi")
     public void us06_Tc01() {
-        ExtentTest extentTest = RaporlamaUtil.extentTest;
 
         //Belirtilen URL'e gidilir.
         //Sign in butonuna tiklanir.
@@ -45,7 +39,7 @@ public class UserStory_06 {
         //Password bilgileri girilir.
         //Sign in butonuna tiklanir.
         loginPage.login();
-        extentTest.info("Login işlemi yapıldı.");
+        RaporlamaUtil.extentTestInfo("Login işlemi yapıldı.");
 
         //Search cubuguna tiklanir.Arama yapilmak istenen urunun ismi girilir.
         //Arama butonuna tiklanir.
@@ -53,18 +47,18 @@ public class UserStory_06 {
 
         //Istenen urune ait arama yapildigi dogrulanir.
         shoppingPage.checkSearchProduct();
-        extentTest.info("Istenen urune ait arama yapildigi dogrulandı.");
+        RaporlamaUtil.extentTestInfo("Istenen urune ait arama yapildigi dogrulandı.");
+        RaporlamaUtil.message = "<span style='color:green; font-weight:bold; font-size: 14px'>TEST SONUCU: </span><br><span style='color:purple; font-size: 16px'>" + raporMesaji + "</span>";
 
     }
 
-    @Test(testName = testName, description = "<span style='font-weight:bold'>Amaç:</span> " + "ADD TO CART butonunun islevsel kontrolu")
+    @Test (testName = "ADD TO CART Buton Test", description = "<span style='font-weight:bold'>Amaç:</span> " + "ADD TO CART butonunun islevsel kontrolu")
     public void us06_Tc02() {
-        ExtentTest extentTest = RaporlamaUtil.extentTest;
 
         //Belirtilen URL'e gidilir.
         //Login islemi yapilir.
         loginPage.login();
-        extentTest.info("Login işlemi yapıldı.");
+        RaporlamaUtil.extentTestInfo("Login işlemi yapıldı.");
 
         //Search inputuna urun ismi girilir ve arama butonuna tiklanir.
         homePage.searchProduct();
@@ -74,53 +68,51 @@ public class UserStory_06 {
 
         //ADD TO CART butonunun tiklanir.
         productPage.addToCartBtnLoc.click();
-        extentTest.info("Sepete ürün eklendi");
+        RaporlamaUtil.extentTestInfo("Sepete ürün eklendi");
 
         //Popup mesajının görüntülendigi dogrulanir.
         Assert.assertTrue(productPage.popupViewCartLoc.isDisplayed());
-        extentTest.info("Sepete ürün eklemesine dair popup mesajı görüntülendi.");
+        RaporlamaUtil.extentTestInfo("Sepete ürün eklemesine dair popup mesajı görüntülendi.");
 
     }
 
-    @Test(testName = testName, description = "<span style='font-weight:bold'>Amaç:</span> " + "Sepete eklenen urunlerin goruntulenmesi")
+    @Test (testName = "Product Add to Cart Test", description = "<span style='font-weight:bold'>Amaç:</span> " + "Sepete eklenen urunlerin goruntulenmesi")
     public void us06_Tc03() {
-        ExtentTest extentTest = RaporlamaUtil.extentTest;
 
         //Belirtilen URL'e gidilir.
         //Login islemi yapilir.
         loginPage.login();
-        extentTest.info("Login işlemi yapıldı.");
+        RaporlamaUtil.extentTestInfo("Login işlemi yapıldı.");
 
         //Search inputuna urun ismi girilir ve arama butonuna tiklanir.
         homePage.searchProduct();
 
         //Sayfada ki ilk urune tiklanilir ve urun sepete eklenir.
         productPage.firstProductAddCart();
-        extentTest.info("Sepete ürün eklendi");
+        RaporlamaUtil.extentTestInfo("Sepete ürün eklendi");
 
         //Cart > VIEW CART sayfasina gidilir.
         productPage.goToViewCart();
 
         //Sepete eklenen urunlerin goruntulendigi dogrulanir.
         Assert.assertTrue(cartPage.productDisplayLoc.isDisplayed());
-        extentTest.info("Sepete eklenen ürünler görüntülendi.");
+        RaporlamaUtil.extentTestInfo("Sepete eklenen ürünler görüntülendi.");
     }
 
-    @Test(testName = testName, description = "<span style='font-weight:bold'>Amaç:</span> " + "Sepetteki urun mıktarında artirma ve azaltma yapilabilmesi")
+    @Test (testName = "Product Plus and Minus Test", description = "<span style='font-weight:bold'>Amaç:</span> " + "Sepetteki urun mıktarında artirma ve azaltma yapilabilmesi")
     public void us06_Tc04() {
-        ExtentTest extentTest = RaporlamaUtil.extentTest;
 
         //Belirtilen URL'e gidilir.
         //Login islemi yapilir.
         loginPage.login();
-        extentTest.info("Login işlemi yapıldı.");
+        RaporlamaUtil.extentTestInfo("Login işlemi yapıldı.");
 
         //Search inputuna urun ismi girilir ve arama butonuna tiklanir.
         homePage.searchProduct();
 
         //Sayfada ki ilk urune tiklanilir ve urun sepete eklenir.
         productPage.firstProductAddCart();
-        extentTest.info("Sepete ürün eklendi");
+        RaporlamaUtil.extentTestInfo("Sepete ürün eklendi");
 
         //Cart > VIEW CART sayfasina gidilir.
         productPage.goToViewCart();
@@ -132,24 +124,23 @@ public class UserStory_06 {
         //Quantity alaninda urun miktari azaltilir.
         //UPDATE CART butonuna tiklanir ve azaltma yapildigi dogrulanir.
         cartPage.checkMinusUpdate();
-        extentTest.info("Sepete eklenen ürünleri artırma ve azaltma durumları kontrol edildi.");
+        RaporlamaUtil.extentTestInfo("Sepete eklenen ürünleri artırma ve azaltma durumları kontrol edildi.");
     }
 
-    @Test(testName = testName, description = "<span style='font-weight:bold'>Amaç:</span> " + "Urun satin alabilmek icin fatura adresinin eksiksiz girilmesi")
+    @Test (testName = "Billing Address Test", description = "<span style='font-weight:bold'>Amaç:</span> " + "Urun satin alabilmek icin fatura adresinin eksiksiz girilmesi")
     public void us06_Tc05() {
-        ExtentTest extentTest = RaporlamaUtil.extentTest;
 
         //Belirtilen URL'e gidilir.
         //Login islemi yapilir.
         loginPage.login();
-        extentTest.info("Login işlemi yapıldı.");
+        RaporlamaUtil.extentTestInfo("Login işlemi yapıldı.");
 
         //Search inputuna urun ismi girilir ve arama butonuna tiklanir.
         homePage.searchProduct();
 
         //Sayfada ki ilk urune tiklanilir ve urun sepete eklenir.
         productPage.firstProductAddCart();
-        extentTest.info("Sepete ürün eklendi");
+        RaporlamaUtil.extentTestInfo("Sepete ürün eklendi");
 
         //Cart > VIEW CART sayfasina gidilir.
         productPage.goToViewCart();
@@ -165,24 +156,23 @@ public class UserStory_06 {
 
         //Popup - uyari mesaji goruntulendigi dogrulanir.
         Assert.assertTrue(checkOutPage.placeOrderPopupMessages.isDisplayed());
-        extentTest.info("Biling Detailes bolumunda zorunlu alanlar boş bırakıldığında hata mesajı görüntülendi");
+        RaporlamaUtil.extentTestInfo("Biling Detailes bolumunda zorunlu alanlar boş bırakıldığında hata mesajı görüntülendi");
     }
 
-    @Test(testName = testName, description = "<span style='font-weight:bold'>Amaç:</span> " + "Odeme seceneklerinin islevsel kontrolu")
+    @Test (testName = "Payment Options Test", description = "<span style='font-weight:bold'>Amaç:</span> " + "Odeme seceneklerinin islevsel kontrolu")
     public void us06_Tc06() {
-        ExtentTest extentTest = RaporlamaUtil.extentTest;
 
         //Belirtilen URL'e gidilir.
         //Login islemi yapilir.
         loginPage.login();
-        extentTest.info("Login işlemi yapıldı.");
+        RaporlamaUtil.extentTestInfo("Login işlemi yapıldı.");
 
         //Search inputuna urun ismi girilir ve arama butonuna tiklanir.
         homePage.searchProduct();
 
         //Sayfada ki ilk urune tiklanilir ve urun sepete eklenir.
         productPage.firstProductAddCart();
-        extentTest.info("Sepete ürün eklendi");
+        RaporlamaUtil.extentTestInfo("Sepete ürün eklendi");
 
         //Cart > VIEW CART sayfasina gidilir.
         productPage.goToViewCart();
@@ -193,7 +183,7 @@ public class UserStory_06 {
         //Payment Methods bolumunde 2 ödeme seceneginin oldugu dogrulanir.
         checkOutPage.checkVisibleWireTransferEFT();
         checkOutPage.checkVisiblePayAtTheDoorBtn();
-        extentTest.info("Ödeme seçeneklerinin görünürlüğü kontrol edildi");
+        RaporlamaUtil.extentTestInfo("Ödeme seçeneklerinin görünürlüğü kontrol edildi");
 
         //Pay at the door secenegi secilir.
         checkOutPage.selectPayAtTheDoor();
@@ -206,24 +196,23 @@ public class UserStory_06 {
 
         //Wire transfer/EFT seceneginin secilebildigi dogrulanir.
         checkOutPage.checkSelectWireTransferEFT();
-        extentTest.info("Ödeme seçeneklerinin seçilebilirliği kontrol edildi");
+        RaporlamaUtil.extentTestInfo("Ödeme seçeneklerinin seçilebilirliği kontrol edildi");
     }
 
-    @Test(testName = testName, description = "<span style='font-weight:bold'>Amaç:</span> " + "Fatura adresi eksiksiz girilerek alisverisin tamamlanmasi ")
+    @Test(testName = "End to Shopping Test", description = "<span style='font-weight:bold'>Amaç:</span> " + "Fatura adresi eksiksiz girilerek alisverisin tamamlanmasi ")
     public void us06_Tc07() {
-        ExtentTest extentTest = RaporlamaUtil.extentTest;
 
         //Belirtilen URL'e gidilir.
         //Login islemi yapilir.
         loginPage.login();
-        extentTest.info("Login işlemi yapıldı.");
+        RaporlamaUtil.extentTestInfo("Login işlemi yapıldı.");
 
         //Search inputuna urun ismi girilir ve arama butonuna tiklanir.
         homePage.searchProduct();
 
         //Sayfada ki ilk urune tiklanilir ve urun sepete eklenir.
         productPage.firstProductAddCart();
-        extentTest.info("Sepete ürün eklendi");
+        RaporlamaUtil.extentTestInfo("Sepete ürün eklendi");
 
         //Cart > VIEW CART sayfasina gidilir.
         productPage.goToViewCart();
@@ -249,7 +238,7 @@ public class UserStory_06 {
         //Phone * inputu doldurulur.
         //Email address * inputu doldurulur.
         checkOutPage.phoneAndMailRead();
-        extentTest.info("Biling Detailes bölümünde zorunlu alanlar eksiksiz dolduruldu ");
+        RaporlamaUtil.extentTestInfo("Biling Detailes bölümünde zorunlu alanlar eksiksiz dolduruldu ");
 
         //Odeme seceneklerinden secim yapilir.
         //PLACE ORDER butonu tılklanir.
@@ -257,14 +246,13 @@ public class UserStory_06 {
 
         //"Thank you. Your order has been received." mesaji goruntulendigi dogrulanir.
         orderCompletePage.checkShoopingEnd();
-        extentTest.info("Alışveriş'in tamamlandığı görüntülendi ");
+        RaporlamaUtil.extentTestInfo("Alışveriş'in tamamlandığı görüntülendi ");
     }
 
     @AfterMethod
     public void afterTest(){
         Driver.closeDriver();
         RaporlamaUtil.message = "<span style='color:green; font-weight:bold; font-size: 14px'>TEST SONUCU: </span><br><span style='color:purple; font-size: 16px'>" + raporMesaji + "</span>";
-
     }
 
 }
