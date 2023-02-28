7c488bfb5071f1b94c255a48c3ff50566c0b47e4
Gürkay Birinci <117175953+gurkaybirinci@users.noreply.github.com>
2023-02-28 11:32:15 +0300

    2023.02.28

--- src/test/java/team8_testngproject/pages/P18_VendorProductManagerPage.java
+++ src/test/java/team8_testngproject/pages/P18_VendorProductManagerPage.java
@@ -359,7 +359,6 @@ public class P18_VendorProductManagerPage {
     @FindBy(xpath = "(//*[@class='select2-search__field'])[5]") public WebElement linkedCrossCellsDropdown2Rs;
     @FindBy(xpath = "(//input[@placeholder='Filter by product ...'])[3]") public WebElement linkedCrossCellsTexboxRs;
     @FindBy(xpath = "//*[@id='select2-crosssell_ids-results']") public WebElement linkedCrossCellsDropdownRs;
-    @FindBy(xpath = "(//*[@class='select2-search__field'])[5]") public WebElement linkedCrossCellsDropdown2Rs;
     @FindBy(xpath = "//*[text()='SOYLEOYLEBOYLE (Art-123)']") public WebElement boyleElementInUpcellsDropdownRs;
     @FindBy(xpath = "//*[contains(text(), 'SEO')]") public WebElement seoMenuRs;
     @FindBy(xpath = "//*[@id='yoast_wpseo_focuskw_text_input']") public WebElement seoEnterAFocusKewordTexboxRs;
